6a357c7499ecceb96bd710b4df3b92836cc3b37a
Nesli <nesliparma@gmail.com>
2025-06-10 14:10:40 +0200

    listed events

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React, { useEffect, useState } from "react";
+import { Heading, Box } from "@chakra-ui/react";
 
 export const EventsPage = () => {
-  return <Heading>List of events</Heading>;
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    async function fetchEvents() {
+      const response = await fetch("http://localhost:3000/events");
+      const events = await response.json();
+      setEvents(events);
+    }
+
+    fetchEvents();
+  }, []);
+
+  return (
+    <Box>
+      <Heading>List of events</Heading>
+      <ul>
+        {events.map((event) => {
+          return <li key={event.id}>- {event.title}</li>;
+        })}
+      </ul>
+    </Box>
+  );
 };
